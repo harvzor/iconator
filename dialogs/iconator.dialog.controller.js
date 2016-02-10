7bad1f24601ceb44b9a47e47f6e0eeea6e0f908a
Harvey Williams <harvey@growcreate.co.uk>
2016-02-10 11:26:41 +0000

    Initial commit.

---
+++ dialogs/iconator.dialog.controller.js
@@ -0,0 +1,13 @@
+//used for the icon picker dialog
+angular.module("umbraco")
+	.controller("Iconator.Dialog.Controller", function ($scope) {
+		/* <= Umbraco 7.4
+		$scope.icons = $scope.dialogData;
+
+		// When the user clicks on an icon.
+		$scope.selectIcon = function(icon) {
+			// Send the data back to the callback.
+			$scope.model.submit(icon);
+		};
+		*/
+	});
